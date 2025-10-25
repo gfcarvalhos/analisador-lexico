babe83f6187e7efa26b22d6ed4e8c5d7e69eb113
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-10-25 14:01:48 -0300

    chore: add runFile function logic

--- lexo/src/main/java/br/ufma/lexico/Lox.java
+++ lexo/src/main/java/br/ufma/lexico/Lox.java
@@ -1,6 +1,9 @@
 package br.ufma.lexico;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Lox {
     public static void main(String[] args) throws IOException {
@@ -13,8 +16,13 @@ public class Lox {
         }
     }
 
-    public static void runFile(String e) {
-        System.out.println("runFile function");
+    public static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    public static void run(String e){
+        System.out.println("run function");
     }
 
     public static void runPrompt(){
