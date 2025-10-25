790511f1bfe7d0bb473e0aec69c21fc2408ee25e
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-10-25 14:28:05 -0300

    feat: add Lox error handler functions

--- lexo/src/main/java/br/ufma/lexico/Lox.java
+++ lexo/src/main/java/br/ufma/lexico/Lox.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script");
@@ -22,6 +24,8 @@ public class Lox {
     public static void runFile(String path) throws IOException{
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if(hadError) System.exit(65);
     }
 
     public static void runPrompt() throws IOException{
@@ -43,5 +47,16 @@ public class Lox {
         for (Token token: tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message){
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message
+        );
+        hadError = true;
     }
 }
