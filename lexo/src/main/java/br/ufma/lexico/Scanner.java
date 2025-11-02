b7395b7e7eb17db0b93db25f86a8aae736599244
André Moura <117699977+AndreMouraL@users.noreply.github.com>
2025-11-01 23:45:41 -0300

    4.5 Reconhecendo Lexemas

--- lexo/src/main/java/br/ufma/lexico/Scanner.java
+++ lexo/src/main/java/br/ufma/lexico/Scanner.java
@@ -34,15 +34,35 @@ public class Scanner {
         return current >= source.length();
     }
 
-    
+    //seção 4.5
     private void scanToken() {
-       
+      char c = advance();
+      switch (c) {
+          case '(': addToken(LEFT_PAREN); break;
+          case ')': addToken(RIGHT_PAREN); break;
+          case '{': addToken(LEFT_BRACE); break;
+          case '}': addToken(RIGHT_BRACE); break;
+          case ',': addToken(COMMA); break;
+          case '.': addToken(DOT); break;
+          case '-': addToken(MINUS); break;
+          case '+': addToken(PLUS); break;
+          case ';': addToken(SEMICOLON); break;
+          case '*': addToken(STAR); break; 
+    
+      }
     }
 
-   
-    private char advance() {
-        current++;
-        return source.charAt(current - 1);
-    }
+   private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
 }
 //Conclusão do Analisador Léxico – Interpretador Lox
