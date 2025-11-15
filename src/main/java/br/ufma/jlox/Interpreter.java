68b5963323e37c7014c8ad04ecd7c771b24500ce
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-11-15 16:31:47 -0300

    feat: adiciona classe interpreter para validação de expressoes

---
+++ src/main/java/br/ufma/jlox/Interpreter.java
@@ -0,0 +1,36 @@
+package br.ufma.jlox;
+
+abstract class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return expr.accept(this);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    private boolean isTruthy(Object object){return true}
+
+}
