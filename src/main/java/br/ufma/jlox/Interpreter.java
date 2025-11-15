cd71928a5d66c30b4b942ef672977756237c6244
Gabriel Silva <gabrielfelipecarvalho1@gmail.com>
2025-11-15 17:03:18 -0300

    feat: adiciona logica para detecção de erros durante execução no validador (interpreter)

--- src/main/java/br/ufma/jlox/Interpreter.java
+++ src/main/java/br/ufma/jlox/Interpreter.java
@@ -23,12 +23,16 @@ abstract class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double)right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double)right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double)right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double)right;
             case MINUS:
                 return (double)left - (double)right;
@@ -39,10 +43,12 @@ abstract class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof  String && right instanceof  String) {
                     return (String)left + (String)right;
                 }
-                break;
-            case SLASH:
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+            case SLASH: //Division
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left / (double)right;
-            case STAR:
+            case STAR: //Multiplication
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left * (double)right;
             case BANG_EQUAL: return !isEqual(left, right);
             case EQUAL_EQUAL: return isEqual(left, right);
@@ -59,9 +65,22 @@ abstract class Interpreter implements Expr.Visitor<Object> {
             case BANG:
                 return !isTruthy(right);
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
         }
         return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator,
+                                     Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
     private boolean isTruthy(Object object){
